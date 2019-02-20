0191a2d912f0138ed3d47b0583a5a10acd50ee1f
TStaaden <tstaaden@gmail.com>
2019-02-20 11:15:02 -0800

    Added comments and made the capability to swap work.

--- main.cpp
+++ main.cpp
@@ -3,38 +3,43 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void sortDescending(int,int,int);
+void sortDescending(int&,int&,int&);
 void swap(int&,int&);
 
 int main()
 {
+	//allows the users to enter thier variables
   int numA, numB, numC;
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
-
+   //calls sortDescending()
   sortDescending(numA, numB, numC);
-  
+  //prints out the numbers from greatest to least. 
   cout<<"From greatest to least, they are: ";
   cout<<numA<<","<<numB<<","<<numC<<endl;
   return 0;
 }
-
-void sortDescending(int first, int second, int third)
+// sorts the order by swapping when a number is in the wrong order.
+void sortDescending(int &first, int &second, int &third)
 {
+	//swaps when third is greater than first
   if( first < third )
   {
     swap(first,third);
   }
+  //swaps when second is greater than first
   if( first < second )
   {
     swap(first,second);
   }
+  // swaps when third is greater than second
   if( second < third )
   {
     swap(second,third);
   }
 }
 
+//defines the procedure for a swap 
 void swap(int &first, int &second)
 {
   int temp = first;
